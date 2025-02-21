9b25386c956ddd3ff6f5edca8665293e37a43ace
Rawat-Aashish <rawataashishr@gmail.com>
2025-02-21 20:02:04 +0530

    some fixes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('test', function () {
     return 'this is in v1';
@@ -17,4 +18,12 @@ Route::prefix('/user')->group(function () {
 
 Route::prefix('/require')->group(function () {
     Route::post('{user}/blood', [MailController::class, 'getBlood']);
+});
+
+
+Route::get('/run-migrations', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations and cache cleared successfully!';
 });
